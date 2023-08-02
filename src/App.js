5aed4a7460b4470933cf645bb4618dd0cfb5633a
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-08-02 17:11:02 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { Canvas } from "@react-three/fiber";
+import Polyhedron from "./Polyhedron";
+import * as THREE from "three";
+import { Stats, OrbitControls } from "@react-three/drei";
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [-1, 4, 2.5] }}>
+        <directionalLight position={[1, 1, 1]} />
+        <Polyhedron
+          name="meshBasicMaterial"
+          position={[-3, 1, 0]}
+          material={new THREE.MeshBasicMaterial()}
+        />
+        <Polyhedron
+          name="meshNormalMaterial"
+          position={[-1, 1, 0]}
+          material={new THREE.MeshNormalMaterial()}
+        />
+        <Polyhedron
+          name="meshPhongMaterial"
+          position={[1, 1, 0]}
+          material={new THREE.MeshPhongMaterial()}
+        />
+        <Polyhedron
+          name="meshStandardMaterial"
+          position={[3, 1, 0]}
+          material={new THREE.MeshStandardMaterial()}
+        />
+        <OrbitControls target-y={1} />
+        <axesHelper args={[5]} />
+        <gridHelper />
+        <Stats />
+      </Canvas>
+    </div>
+  );
+}
